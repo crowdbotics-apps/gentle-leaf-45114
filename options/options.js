1809eff9093cfa03b64b9c59878e06c3ec651334
Crowdbotics <team@crowdbotics.com>
2023-12-19 20:40:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-leaf-45114",
+  url: "https://gentle-leaf-45114.botics.co",
+  api: "https://gentle-leaf-45114.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
